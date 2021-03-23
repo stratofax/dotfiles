e9b16a5c64d4368c30a137f95dcdda9251ab14a5
Neil Johnson <neil@cadent.com>
2021-03-22 22:10:54 -0400

    New abbr's

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
+if status --is-interactive
+    abbr --add --global dtf dotfiles 
+    abbr --add --global dtfs dotfiles status
+    abbr --add --global dtfa dotfiles add 
+    abbr --add --global dtfc dotfiles commit -m 
+    abbr --add --global gco git checkout
+    # etcetera
+end
+
 # eval (pipenv --completion)
 if test (uname -s) = "Darwin"
   set -gx PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
