6bcbd13eda4d62da3f508d7fe7d52544914ebc2f
Neil Johnson <neil@cadent.com>
2023-10-14 17:31:17 -0400

    Hide untracked files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status --is-interactive
     abbr --add --global dtf 'dotfiles'
-    abbr --add --global dtfs 'dotfiles status'
+    abbr --add --global dtfs 'dotfiles status -uno'
     abbr --add --global dtfa 'dotfiles add'
     abbr --add --global dtfc 'dotfiles commit -m'
     abbr --add --global dtfp 'dotfiles push'
